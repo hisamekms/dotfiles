d940981911dd1fef93f5c5550b5e21aab01c261a
Shinnosuke Ooyama <s.ooyama1003@gmail.com>
2014-10-29 10:17:24 +0900

    Fix ... aliases

--- zsh/.zsh/plugins/aliases.zsh
+++ zsh/.zsh/plugins/aliases.zsh
@@ -5,8 +5,9 @@ alias -g @x='| xargs'
 alias pu=pushd
 alias po=pushd
 
-alias -g ...=../../..
-alias -g ....=../../../..
-alias -g .....=../../../../..
+alias -g ...=../..
+alias -g ....=../../..
+alias -g .....=../../../..
+alias -g ......=../../../../..
 
 alias reload="exec -l $SHELL"
