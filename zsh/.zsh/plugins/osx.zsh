a7a93635c8dd592aac717ef11dbfc6a81e8b831e
Shinnosuke Ooyama <s.ooyama1003@gmail.com>
2014-11-07 11:51:01 +0900

    Change function name in osx.zsh

--- zsh/.zsh/plugins/osx.zsh
+++ zsh/.zsh/plugins/osx.zsh
@@ -2,7 +2,7 @@ export DEVELOPER_DIR="/Applications/XCode.app/Contents/Developer"
 
 alias symbolicatecrash="/Applications/Xcode.app/Contents/SharedFrameworks/DTDeviceKitBase.framework/Versions/A/Resources/symbolicatecrash"
 
-function brew_cask_upgrade() {
+function brew-cask-upgrade() {
   for c in `brew cask list`; do
     ! brew cask info $c | grep -qF "Not installed" || brew cask install $c
   done
