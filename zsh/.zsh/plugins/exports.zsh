26ccd73959b43f67efc816e4ca71baefa7fd9511
Shinnosuke Ooyama <s.ooyama1003@gmail.com>
2014-11-15 23:54:54 +0900

    Fix type GOTPAHT -> GOPATH

--- zsh/.zsh/plugins/exports.zsh
+++ zsh/.zsh/plugins/exports.zsh
@@ -5,13 +5,19 @@ export KCODE=u
 # export AUTOFEATURE=true  # autotestでfeatureを動かす
 
 export PATH=/usr/local/bin:$PATH
-export PATH="$HOME/.anyenv/bin:$PATH"
-eval "$(anyenv init -)"
+
+if [ -e ~/.anyenv ]; then
+  export PATH="$HOME/.anyenv/bin:$PATH"
+  eval "$(anyenv init -)"
+else
+  function install-anyenv () {
+    git clone https://github.com/riywo/anyenv ~/.anyenv
+  }
+fi
 # export PATH=$HOME/.nodebrew/current/bin:$PATH
 export PATH=$PATH:~/go/bin
 
-
-export GOTPATH=~/go
+export GOPATH=~/go
 
 export ANDROID_HOME=/usr/local/opt/android-sdk
 
