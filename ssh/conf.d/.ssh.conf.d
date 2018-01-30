99e8ddf9dbb372e55f87de51ee0dad70b43776f1
hisamekms <s.ooyama1003@gmail.com>
2018-01-31 08:57:17 +0900

    delete all

--- ssh/conf.d/.ssh.conf.d
+++
@@ -1,1 +0,0 @@
-/Users/ooyamashinnosuke/.ghq/github.com/hisamekms/dotfiles/ssh/.ssh.conf.d
--- zsh/plugins/docker.zsh
+++
@@ -1,2 +0,0 @@
-alias docker-stop-all="docker stop \$(docker ps -aq)"
-alias docker-rm-all="docker rm -f \$(docker ps -aq)"
