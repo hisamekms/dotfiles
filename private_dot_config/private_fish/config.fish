0563ecf2de3a9125fd66dcd0a137c8fef54cadb9
hisamekms <s.ooyama1003@gmail.com>
2026-01-07 09:54:37 +0900

    fish - brewの設定を追加する

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -10,6 +10,10 @@ if command -v direnv >/dev/null
   eval (direnv hook fish)
 end
 
+if test -x /opt/homebrew/bin/brew
+  /opt/homebrew/bin/brew shellenv | source
+end
+
 set -U GHQ_SELECTOR peco
 set -x EDITOR vim
 
@@ -55,27 +59,12 @@ function ghq_peco
     end
 end
 
-function __fzf_wtp_cd
-    set -l selected
-    if command -v wtp >/dev/null 2>&1; and command -v fzf >/dev/null 2>&1
-        set selected (wtp list 2>/dev/null | awk 'NR>2 && NF>0 {print $1}' | fzf --height 40% --border)
-        if test -n "$selected"
-            if test "$selected" = "@"
-                cd "/workspace"
-            else
-                cd "/workspace/worktrees/$selected"
-            end
-        end
-    end
-end
-
 function fish_user_key_bindings
   # unbind fish-ghq key bindings
   bind -e \cg
   bind \cr 'ghq_peco'
   bind \cq '__peco_z'
   bind \ch peco_select_history
-  bind \cw '__fzf_wtp_cd'
 end
 
 mise activate fish | source
@@ -94,7 +83,4 @@ abbr -a gplr 'git pull --rebase'
 
 # Others
 abbr -a m 'mise'
-abbr -a mr 'mise run'
 abbr -a che 'chezmoi'
-
-wtp shell-init fish | source
