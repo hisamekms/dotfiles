3b05ae1b5a9b7fd39bceaa41b003874308f73c2d
hisamekms <s.ooyama1003@gmail.com>
2026-01-10 15:36:33 +0900

    fish - 不要になったファイルを削除する

--- private_dot_config/private_fish/conf.d/10_fisher.fish
+++
@@ -1,5 +0,0 @@
-if not functions -q fisher
-    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
-    curl -sL https://git.io/fisher | source
-    fisher install < $XDG_CONFIG_HOME/fish/fish_plugins
-end
