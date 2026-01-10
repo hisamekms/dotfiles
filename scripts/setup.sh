91ce1e9bf5ed9665df9484b08c4c7ab252155be8
hisamekms <s.ooyama1003@gmail.com>
2026-01-10 16:28:44 +0900

    scripts  - setup aptをsudoで実行する

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-apt update
-apt install -y fish
+sudo apt update
+sudo apt install -y fish
 
 if command -v chsh >/dev/null 2>&1; then
   chsh -s "$(command -v fish)" || true
