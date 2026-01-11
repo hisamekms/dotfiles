81086e77276c5a89361fd1d4f7108ab61e7c118f
hisamekms <s.ooyama1003@gmail.com>
2026-01-11 09:06:28 +0900

    scripts - setupを改善する

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,13 +5,10 @@ set -euo pipefail
 if ! command -v fish &> /dev/null; then
     sudo apt update
     sudo apt install -y fish
-    
-    # fishインストール後にディレクトリの所有権を修正
-    if [ -d "$HOME/.local/share/fish" ]; then
-        sudo chown -R "$(whoami):$(whoami)" "$HOME/.local/share/fish"
-    fi
 
-    sudo chsh -s "$(command -v fish)" "$(whoami)" || true
+    sudo chown "$(whoami):$(whoami)" "$HOME/.local"
+
+    sudo chsh -s "$(command -v fish)" "$(whoami)"
 fi
 
 
