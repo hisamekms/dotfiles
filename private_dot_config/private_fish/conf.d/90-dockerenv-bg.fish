96b962ee961253a2bcd551c34ed5b77107dd9814
hisamekms <s.ooyama1003@gmail.com>
2026-02-24 23:26:43 +0900

    fish - docker container内だと背景を変える

---
+++ private_dot_config/private_fish/conf.d/90-dockerenv-bg.fish
@@ -0,0 +1,11 @@
+if status is-interactive
+    if test -f /.dockerenv
+        printf '\e]11;#0f2d3a\a'
+
+        function __dockerenv_restore_bg_on_exit --on-event fish_exit
+            # Reset terminal background to its default (supported terminals).
+            printf '\e]111\a'
+            functions -e __dockerenv_restore_bg_on_exit
+        end
+    end
+end
